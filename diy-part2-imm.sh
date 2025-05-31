05315d695ef21d7640fe37b55d525d6a8da6a170
yer2018 <42699536+yer2018@users.noreply.github.com>
2025-05-31 08:20:44 +0800

    Update diy-part2-imm.sh

--- diy-part2-imm.sh
+++ diy-part2-imm.sh
@@ -23,7 +23,8 @@ sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 sed -i 's#openwrt.proxy.ustclug.org#mirrors.bfsu.edu.cn\\/openwrt#' package/lean/default-settings/files/zzz-default-settings
 
 sed -i 's/invalid users = root/#invalid users = root/g' feeds/packages/net/samba4/files/smb.conf.template
-
+sed -i 's/\.apdisk\//\.apdisk\/\._*/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/login/login -f root/g' package/feeds/packages/ttyd/files/ttyd.config
 
 # 拉取软件包
 
